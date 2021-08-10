7850c8c5274348dbaaa6cb7a1f47e872dc54b620
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2021-08-10 20:58:32 +0900

    Fix build failure on Cygwin
    Cygwin requires stdio.h for netinet/tcp.h.

--- estnode.c
+++ estnode.c
@@ -13,6 +13,7 @@
  * Boston, MA 02111-1307 USA.
  *************************************************************************************************/
 
+#include <stdio.h>
 #include <netinet/tcp.h>
 #include "estraier.h"
 #include "estmtdb.h"
